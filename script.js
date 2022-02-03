57b2bd634b7bf01390031170e64641369b4e7868
daniel3-dev <annoyingfish2@gmail.com>
2022-02-02 19:38:18 -0600

    Finished carousel, compressed images

--- script.js
+++ script.js
@@ -1,62 +1,71 @@
 //Modal Code ######################################################
+var quoteModal = document.getElementById('quoteModal');
+var quoteModalButton = document.getElementById('quoteModalButton');
+var closeButton = document.getElementsByClassName('close')[0];
 
-var quoteModal = document.getElementById("quoteModal");
-var quoteModalButton = document.getElementById("quoteModalButton");
-var closeButton = document.getElementsByClassName("close")[0];
+quoteModalButton.onclick = function () {
+  quoteModal.style.display = 'block';
+};
 
-quoteModalButton.onclick = function(){
-    quoteModal.style.display = "block";
-}
+closeButton.onclick = function () {
+  quoteModal.style.display = 'none';
+};
 
-closeButton.onclick = function(){
-    quoteModal.style.display = "none";
-}
-
-window.addEventListener("click", function(event) {
-    if(event.target == quoteModal){
-        quoteModal.style.display = "none";
-    }
+window.addEventListener('click', function (event) {
+  if (event.target == quoteModal) {
+    quoteModal.style.display = 'none';
+  }
 });
 
 //Carousel Code ###################################################
+let currentSlide = 1;
+let slideH1 = document.getElementById('slideH1');
+let slideP = document.getElementById('slideP');
+let bio = document.getElementById('bio');
 
-let images = ['Images/founder.jpg', 'Images/carol.png', 'Images/soren.png'];
+showSlide(currentSlide);
 
-const previous = document.getElementById("previous");
-const next = document.getElementById("next");
-const slide = document.getElementById("slide");
-//const currentImage
+function showSlide(n) {
+  let i;
+  const slides = document.getElementsByClassName('slide');
+  const dots = document.getElementsByClassName('fa-circle');
 
-previous.onclick = function previousCard(){
-let currentBackground = window.getComputedStyle(slide, false);
-currentBackground = currentBackground.backgroundImage.slice(4, -1).replace(/"/g, "");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].classList.remove('activeSlide');
+  }
 
-alert(currentBackground);
+  slides[currentSlide - 1].style.display = 'block';
+  dots[currentSlide - 1].classList.add('activeSlide');
+  changeText();
 }
 
-next.onclick = function nextCard(){
-    alert("NEXT");
+function changeSlide(n) {
+  showSlide((currentSlide = n));
 }
 
-/*
-    On next button clicked
-        Get current background image position in array
-        If next image in array
-            Background image = next image
-        Else
-            Background image = first image in array
-
-    On previous button clicked
-        Get current background image position in array
-        If previous image in array
-            Background image = previous image
-        Else
-            Background image = last image in array
-*/
-
-/*previous.addEventListener("click", function(){
-    const currentCard = document.getElementsByClassName("view")[0];
-    const previousCard = 
-});*/
-
-
+function changeText() {
+  if (currentSlide == 1) {
+    slideH1.innerText = 'Professional Service and Trusted for Delivering Goods';
+    slideP.innerText =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pretium fusce id velit ut tortor pretium viverra suspendisse. Id faucibus nisl tincidunt eget nullam non. Felis donec et odio pellentesque diam volutpat commodo. Quis hendrerit dolor magna eget.';
+    bio.innerHTML =
+      '<span style="font-weight: 600">Thomas Wake</span>, Founder & CEO of <span style="font-weight: 900">LOGISTICS</span>';
+  } else if (currentSlide == 2) {
+    slideH1.innerText =
+      'I have had nothing but good experiences with LOGISTICS and I really appreciate the brand';
+    slideP.innerText =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Pellentesque diam volutpat commodo sed. Purus semper eget duis at tellus at urna. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a. Volutpat sed cras ornare arcu dui vivamus arcu felis bibendum.';
+    bio.innerHTML =
+      '<span style="font-weight: 600">Carol Tomé</span>, CEO of <span style="font-weight: 900">UPS</span>';
+  } else if (currentSlide == 3) {
+    slideH1.innerText =
+      'The people at LOGISTICS are extremely helpful and knowledgeable in their job';
+    slideP.innerText =
+      'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Integer eget aliquet nibh praesent tristique magna sit amet purus. Tristique et egestas quis ipsum suspendisse ultrices gravida dictum fusce. Dolor sed viverra ipsum nunc. Purus faucibus ornare suspendisse sed nisi.';
+    bio.innerHTML =
+      '<span style="font-weight: 600">Søren Skou</span>, CEO of <span style="font-weight: 900">Maersk</span>';
+  }
+}
